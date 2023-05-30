9f15e458ffc62eaf002d66a02295b525c3858f29
chrisflodin <christopher.flodin@gmail.com>
2023-05-30 19:50:44 +0200

    add loading animation on hero

--- src/pages/HomePage/LandingCanImage.tsx
+++ src/pages/HomePage/LandingCanImage.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion'
 import styles from './LandingCanImage.module.scss'
 import { animationDuration } from './animation-config'
+import { useEffect, useState } from 'react'
 
 interface LandingCanImageProps {
   index: number
@@ -8,9 +9,29 @@ interface LandingCanImageProps {
   anyHovered: boolean
 }
 
-const duration = 0.2 * (Math.random() + 0.7) * animationDuration
-
 function LandingCanImage({ index, flavour, anyHovered }: LandingCanImageProps) {
+  const [hasLoaded, setHasLoaded] = useState<boolean>(false)
+
+  const initialTransition = {
+    ease: 'easeOut',
+    type: 'spring',
+    mass: 10 * Math.random() + 0.2,
+    damping: 30,
+  }
+
+  const duration = 0.2 * (Math.random() + 0.7) * animationDuration
+  const afterLoadedTransition = { duration, ease: 'easeIn' }
+
+  const imageVariants = {
+    initial: { opacity: 0, y: -350 },
+    onLoad: { opacity: 1, y: 0 },
+    afterLoad: { opacity: anyHovered ? 0 : 1, y: anyHovered ? 50 : 0 },
+  }
+
+  useEffect(() => {
+    setHasLoaded(true)
+  }, [])
+
   return (
     <div className={styles.can_image_wrapper}>
       <motion.img
@@ -18,9 +39,10 @@ function LandingCanImage({ index, flavour, anyHovered }: LandingCanImageProps) {
         alt={`Can of ${flavour}`}
         src={`../../assets/images/${flavour}.png`}
         key={index}
-        initial={{ opacity: 1, y: 0 }}
-        animate={{ opacity: anyHovered ? 0 : 1, y: anyHovered ? 50 : 0 }}
-        transition={{ duration, ease: 'easeIn' }}
+        variants={imageVariants}
+        initial="initial"
+        animate={hasLoaded ? 'afterLoad' : 'onLoad'}
+        transition={hasLoaded ? afterLoadedTransition : initialTransition}
       />
     </div>
   )
